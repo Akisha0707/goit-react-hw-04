b3734f9bab651748f85dcabb1b6fae2ea12e8f79
Akisha0707 <Sakimova@gmail.com>
2024-02-06 21:15:07 +0200

    change app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
+// import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-import css from './App.css';
+import css from '/src/components/App/App.module.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
+      <p>hello</p>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -23,7 +24,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p> */}
     </>
   );
 }
